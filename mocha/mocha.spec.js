b45ad1bb521e1efd49f91e2748e13b1853f1677d
Leonard Allain-Launay <leonard@codingame.com>
2017-06-20 15:47:02 -0700

    Minor fixes

--- mocha/mocha.spec.js
+++ mocha/mocha.spec.js
@@ -9,7 +9,7 @@ it('should add ', function() {
         assert.equal(5, mochaExemple.add(2, 2));
         assert.equal(2, mochaExemple.add(-1, -1));
         assert.notEqual(0, mochaExemple.add(0, 0));
-        printMessage('Congratulation !!!', 'You have successfully create a unit test');
+        printMessage('Congratulation !!!', 'You have successfully created a unit test');
     } catch (error) {
         printMessage('Hint 💡', 'Have you checked your assert ?');
         throw error;
